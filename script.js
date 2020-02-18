5a43ad05773014c4cafda4b651b81443b656f5c1
Serg-leichenko <mega-serg2707@ukr.net>
2020-02-18 14:21:56 +0200

    files

--- script.js
+++ script.js
@@ -1,33 +1,51 @@
 var oneHeight = 250, // размер одного элемента
   numImage = 5; // количество элементов
-function LetsGo() {
-  var x = [];
-  for (var i in {
-    fruitBlockImg_1,
-    fruitBlockImg_2,
-    fruitBlockImg_3,
-    fruitBlockImg_4,
-    fruitBlockImg_5
-  }) {
-    x.push({
-      a: document.getElementById(i), // this - это HTML контрол с классом XXX
-      speed: 30 + Math.floor(Math.random() * 20), // скорость
-      scr: Math.floor(Math.random() * oneHeight * numImage) // начальное положение
-    });
-  }
-  var interval = setInterval(function() {
-    var i = x.length,
-      complete = true;
-    while (i--) {
-      var a = x[i];
-      a.scr += a.speed;
-      if (a.speed > 0) {
-        a.speed -= 0.2;
-        complete = false;
+  speedStep = 0.2;
+    function LetsGo(){
+      clearInterval(LetsGo.interval);
+      var x=[];
+      for(var i in{
+        fruitBlockImg_1,
+        fruitBlockImg_2,
+        fruitBlockImg_3,
+        fruitBlockImg_4,
+        fruitBlockImg_5
+      }) {
+        var ob=5+Math.floor(Math.random()*6), // количество оборотов 5 минимум, 10 - максимум
+            nn=Math.random()*numImage;
+        num=Math.floor(nn), // выигравший нумер
+            o={
+              ob:ob,num:num,
+              a:document.getElementById(i), // это наш контрол с классом XXX
+              // время=(speedStart)/speedStep
+              // расстояние=(speedStart/2)*время -арифм прогр
+              // расстояние=speedStart^2/(2*speedStep)
+              // speedStart=sqrt(расстояние*2*speedStep)
+              speed:Math.sqrt(((ob*numImage+num)* oneHeight)* speedStep*2), // скорость
+              scr:-26 // магия, блин
+            }; // выигравший номер
+        x.push(o);
       }
-      if (a.scr > oneHeight * numImage) a.scr -= oneHeight * numImage;
-      a.a.scrollTop = Math.floor(a.scr);
+      LetsGo.interval=setInterval(
+          function (){
+            var i = x.length,complete=true;
+            while(i--){
+              var a=x[i];
+              a.scr+=a.speed;
+              if(a.speed>2*speedStep){
+                a.speed-=speedStep;complete=false;
+              } else if(a.speed>0) {
+                var t= Math.round(a.scr / oneHeight);
+                if(t>=numImage) t=0;
+                console.log(i,t,a.scr-oneHeight*t,a.ob,a.num);
+                a.scr=oneHeight*t;
+                a.speed=0;
+                complete=false;
+              }
+              if(a.scr>(oneHeight*numImage))
+                a.scr-=oneHeight*numImage;
+              a.a.scrollTop=Math.floor(a.scr);
+            }
+            if(complete) clearInterval(LetsGo.interval)
+          },20)
     }
-    if (complete) clearInterval(interval);
-  }, 20);
-}
